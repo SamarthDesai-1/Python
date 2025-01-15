d8f42c0f5a8ab6baca44d3c4d7cff645fc83ab5a
Samarth Desai <101860948+SamarthDesai-1@users.noreply.github.com>
2025-01-15 09:04:48 +0530

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -5,10 +5,4 @@ print(str[-2]) # we can also the string by negative indexes also
 
 print(f"Length of string is : {len(str)}")
 
-print(f"Accessing all characters from the string : {str[0 : len(str)]}") # Slicing the string according to the required indexes
-
-
-
-str = "0123456789"
-print(str[0 : len(str) : 3]) # 0369
 
